7563de6289b1c0abd7124a9d436a6e8df063ce6a
Laurent Willer <lwiller@z1r4p5.42lyon.fr>
2020-11-26 04:19:52 -0800

    first commit

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwiller <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/12 16:35:09 by lwiller           #+#    #+#             */
+/*   Updated: 2020/09/12 17:39:24 by lwiller          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				ft_swap(&tab[i], &tab[j]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
